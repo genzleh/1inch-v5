ac3e7dd13e3ea20970e191f0f26ec30a1033cc7a
0xMYF <96159956+0xMYF@users.noreply.github.com>
2022-12-17 21:28:44 +0700

    Create approval.js

---
+++ approval.js
@@ -0,0 +1,52 @@
+const Web3 = require('web3')
+const axios = require('axios')
+require('dotenv').config()
+
+const RPC_URL = process.env.RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+const web3 = new Web3(RPC_URL)
+const wallet = web3.eth.accounts.wallet.add(PRIVATE_KEY)
+
+async function approve(tokenAddress, tokenAmount){
+    try{
+        const response = await axios.get(`https://api.1inch.io/v5.0/137/approve/transaction?tokenAddress=${tokenAddress}&amount=${tokenAmount}`)
+        if(response.data){
+            data = response.data
+            data.gas = 1000000
+            data.from = wallet.address
+            tx = await web3.eth.sendTransaction(data)
+            if(tx.status){
+                console.log("Approval Berhasil !!!")
+            }else{
+                console.log("Approval Gagal !!!")
+                console.log(tx)
+            }
+        }
+    }catch(err){
+        console.log("could not approve token")
+        console.log(err)
+    }
+}
+
+async function swapper(fromTokenAddress, fromTokenAmount){
+    try{
+        if(fromTokenAddress!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'){
+            await approve(fromTokenAddress, fromTokenAmount)
+
+        }
+    }catch(err){
+        console.log("swapper encountered an error below")
+        console.log(err)
+    }
+
+}
+async function main(){
+
+    fromTokenAddress = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F' // USDT Contract Address sesuaikan
+    fromTokenAmount = '100000000000000' 
+
+    await swapper(fromTokenAddress, fromTokenAmount)
+}
+
+main()
